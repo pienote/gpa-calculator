3e63351972416396488369634f5722969598ebae
pienote <pienote1234567@gmail.com>
2016-11-17 17:39:48 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button')
-        .addEventListener('click', main);
+document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('button').addEventListener('click', main);      
 });
- 
+
 function main() {
-    chrome.tabs.executeScript(null, {
-        file: "gpa_calc.js"
-    });
+	chrome.tabs.executeScript(null, {file: "jquery.js"});
+	chrome.tabs.executeScript(null, {file: "gpa_calc.js"});
 };
